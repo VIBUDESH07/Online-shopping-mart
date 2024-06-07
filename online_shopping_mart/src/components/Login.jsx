11b12127a7b9f8a1728f9abdc942777c6009809e
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-06-07 09:21:06 +0530

    Create Login.jsx

---
+++ online_shopping_mart/src/components/Login.jsx
@@ -0,0 +1,71 @@
+// LoginPage.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import '../Styles/Auth.css'; // Assuming a separate CSS file for authentication styles
+
+const Login= () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const validate = () => {
+    const errors = {};
+    if (!formData.email) errors.email = 'Email is required';
+    if (!formData.password) errors.password = 'Password is required';
+    return errors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      // Handle successful form submission
+      console.log('Form data submitted:', formData);
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        
+        <div className="form-group">
+          <label>
+            <FontAwesomeIcon icon={faEnvelope} /> Email
+          </label>
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+        
+        <div className="form-group">
+          <label>
+            <FontAwesomeIcon icon={faLock} /> Password
+          </label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+          {errors.password && <p className="error">{errors.password}</p>}
+        </div>
+        
+        <button type="submit" className="auth-button">Login</button>
+        <p className='auth-p'>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
