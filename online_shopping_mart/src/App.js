facfd3bfab9bef08b59f2d02ddc5e9f7bf6a49fb
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-06-11 20:15:55 +0530

    Update App.js

--- online_shopping_mart/src/App.js
+++ online_shopping_mart/src/App.js
@@ -7,13 +7,14 @@ import AddProduct from './components/AddProduct';
 import Product from './components/Product';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import Details from './components/Details';
 
 function App() {
   return (
     <Routes>
       {/* Route to the Header component */}
       <Route path="/" element={<Header />} />
-
+      <Route path="/product/:id" element={<Details/>}/>
       {/* Route to the AddProduct component */}
       <Route path="/add" element={<AddProduct />} />
       <Route path="/signup" element={<SignUp />} />
